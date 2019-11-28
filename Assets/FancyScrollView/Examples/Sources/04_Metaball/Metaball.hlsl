8b6bc66dc35ae041f9dc4a72893c76068fbf3c87
setchi <fox@setchi.jp>
2019-11-28 22:31:46 +0900

    Add copyright

--- Assets/FancyScrollView/Examples/Sources/04_Metaball/Metaball.hlsl
+++ Assets/FancyScrollView/Examples/Sources/04_Metaball/Metaball.hlsl
@@ -1,4 +1,10 @@
-﻿#ifndef GALLERY_METABALL_HLSL_INCLUDED
+﻿/*
+ * FancyScrollView (https://github.com/setchi/FancyScrollView)
+ * Copyright (c) 2019 setchi
+ * Licensed under MIT (https://github.com/setchi/FancyScrollView/blob/master/LICENSE)
+ */
+
+#ifndef GALLERY_METABALL_HLSL_INCLUDED
 #define GALLERY_METABALL_HLSL_INCLUDED
 
 #define CELL_COUNT 5 // CeilToInt(1f / cellInterval)
--- Assets/FancyScrollView/Examples/Sources/04_Metaball/Metaball.shader
+++ Assets/FancyScrollView/Examples/Sources/04_Metaball/Metaball.shader
@@ -1,4 +1,10 @@
-﻿Shader "FancyScrollViewGallery/Metaball"
+﻿/*
+ * FancyScrollView (https://github.com/setchi/FancyScrollView)
+ * Copyright (c) 2019 setchi
+ * Licensed under MIT (https://github.com/setchi/FancyScrollView/blob/master/LICENSE)
+ */
+
+Shader "FancyScrollViewGallery/Metaball"
 {
     Properties
     {
